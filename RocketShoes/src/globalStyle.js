62ae7b91a8aba1f05cd00db47de8c630a526d928
Lucas Andrade <lucasantos.dev@gmail.com>
2022-02-18 18:41:03 -0300

    adding global styles

---
+++ RocketShoes/src/globalStyle.js
@@ -0,0 +1,18 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      'html, body': {
+        width: '100%',
+        height: '100%'
+      },
+      'body': {
+        backgroundColor: '#E5E5E5',
+        fontFamily: 'Roboto'
+      }
+    }
+  }
+})
+
+export default theme
